ae5ae7c153f34ea8afcaaf43eb4253cba1c07d27
KR00446203 <KR00446203@techmahindra.com>
2016-07-11 16:31:41 +0530

    gesture added

--- app/pages/gestures/gestures.ts
+++ app/pages/gestures/gestures.ts
@@ -5,7 +5,23 @@ import {NavController, MenuController} from 'ionic-angular';
   templateUrl: 'build/pages/gestures/gestures.html'
 })
 export class GesturesPage {
+  public press: number = 0;
+  public pan: number = 0;
+  public swipe: number = 0;
+  public tap: number = 0;
   constructor(private navController: NavController,menu: MenuController) {
     menu.enable(true);
+  }
+  pressEvent(e) {
+    this.press++
+  }
+  panEvent(e) {
+    this.pan++
+  }
+  swipeEvent(e) {
+    this.swipe++
+  }
+  tapEvent(e) {
+    this.tap++
   }
 }
